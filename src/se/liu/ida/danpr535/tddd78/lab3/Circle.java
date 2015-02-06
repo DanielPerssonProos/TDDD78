cd1b7cbe25a48d54e1d16e80b1cd80afca983292
Daniel Persson Proos <danpr535@student.liu.se>
2015-02-06 14:52:56 +0100

    Added Circle and TestCircle classes

---
+++ src/se/liu/ida/danpr535/tddd78/lab3/Circle.java
@@ -0,0 +1,20 @@
+package se.liu.ida.danpr535.tddd78.lab3;
+
+import java.awt.Color;
+
+/**
+ * Created by Daniel on 15-02-06.
+ */
+public class Circle {
+    public int x;
+    public int y;
+    public int radius;
+    public Color color;
+
+    public Circle(int x, int y, int radius, Color color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+}
---
+++ src/se/liu/ida/danpr535/tddd78/lab3/TestCircle.java
@@ -0,0 +1,12 @@
+package se.liu.ida.danpr535.tddd78.lab3;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Daniel on 15-02-06.
+ */
+public class TestCircle {
+    public static void main(String[] args) {
+        ArrayList<Circle> circleList = new ArrayList<Circle>();
+    }
+}
